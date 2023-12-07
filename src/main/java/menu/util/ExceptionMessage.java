352a6fc9821e40790e39cbaaf9af58e011ed2121
noyes5 <noyes5@naver.com>
2023-12-07 15:13:23 +0900

    style: 코드 컨벤션 수정

--- src/main/java/menu/util/ExceptionMessage.java
+++ src/main/java/menu/util/ExceptionMessage.java
@@ -4,7 +4,6 @@ public enum ExceptionMessage {
     DUPLICATE_COACH_NAME("같은 코치명은 입력할수 없습니다."),
     INVALID_CATEGORY_NAME("카테고리를 찾을 수 없습니다."),
     INVALID_COACH_NAME( "코치 이름은 2글자 이상 4글자 이하여야 합니다."),
-    INVALID_COACH_NUMBER("코치는 최소 2명 이상 입력해야 합니다."),
     INVALID_COACH_SIZE("코치는 2명부터 5명까지 입력 가능합니다."),
     INVALID_INEDIBLE_MENU_SIZE("못 먹는 메뉴는 0개부터 2개까지 입력 가능합니다."),
     INVALID_INPUT("입력은 한글만 가능합니다."),
